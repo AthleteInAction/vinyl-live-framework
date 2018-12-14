c89c0761c0c4e2e9dce1e20ecb03cc2c8d7cec20
onlyexcellence <will@wambl.com>
2018-12-14 03:02:03 -0800

    Initial

---
+++ VinylLive/VinylLive.h
@@ -0,0 +1,19 @@
+//
+//  VinylLive.h
+//  VinylLive
+//
+//  Created by William Robinson on 12/14/18.
+//  Copyright © 2018 Wambl, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VinylLive.
+FOUNDATION_EXPORT double VinylLiveVersionNumber;
+
+//! Project version string for VinylLive.
+FOUNDATION_EXPORT const unsigned char VinylLiveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VinylLive/PublicHeader.h>
+
+
